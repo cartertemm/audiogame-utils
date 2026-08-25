097aba1dee1d86750b522b8b22b3ed0eea5fe259
Carter Temm <cartertemm@gmail.com>
2026-08-25 11:21:57 -0700

    stats.js: NVGT attribution

--- src/stats.js
+++ src/stats.js
@@ -1,4 +1,6 @@
 // Tracks numeric and string stats, formatted output, sorting, and serialization.
+// Originally part of the NVGT scripting language (https://nvgt.dev).
+// Ported to Javascript for use in the audiogame-utils library by Quin Gillespie and Carter Temm
 
 export const STAT_SORT_MODE = {
 	NONE: 'none',
